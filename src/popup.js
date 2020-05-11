21db8f2069dbf75679cc17698e04310009fd0cca
zhouweibo <zhouweibo@beisen.com>
2020-05-11 20:38:12 +0800

    feat: add clear pop

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,25 @@
 let resetButon = document.getElementById('RESET');
 
+const reloadCurrentPage = () => {
+  chrome.tabs.query({active: true, currentWindow: true}, function (arrayOfTabs) {
+    chrome.tabs.reload(arrayOfTabs[0].id);
+ });
+}
 
 resetButon.onclick = function(element) {
-  chrome.storage.sync.clear();
+  // todo: just clear this domain
+  // chrome.storage.sync.clear();
+  const theHost = window.location.origin;
+  chrome.storage.sync.get(theHost, function(res){
+    if(res && res[theHost]){
+      chrome.storage.sync.set({
+        [theHost]: null
+      }, function(res){
+        reloadCurrentPage()
+      })
+    }else{
+      reloadCurrentPage()
+      // chrome.runtime.reload ()
+    }
+  })
 };
